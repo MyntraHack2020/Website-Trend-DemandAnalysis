20a6edb110da869a211ec94fbf22b7f122539509
shivanggarg1998 <shivanggarg1998@gmail.com>
2020-11-18 10:32:26 +0530

    Updates

---
+++ precache-manifest.4ca122c08b72f35ba7df099144a216c0.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09fc489f9139dff504def5cfde5598e6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f95f1fa7e97de5572c1d",
+    "url": "./static/css/0.21c214e0.chunk.css"
+  },
+  {
+    "revision": "321f3872ef21bc616db1",
+    "url": "./static/css/1.b26df3bb.chunk.css"
+  },
+  {
+    "revision": "8d43696490ca4228dd4c",
+    "url": "./static/css/main.d70c0a58.chunk.css"
+  },
+  {
+    "revision": "f95f1fa7e97de5572c1d",
+    "url": "./static/js/0.fab4c4f1.chunk.js"
+  },
+  {
+    "revision": "50657dd44cde48d36e24db270ce065f0",
+    "url": "./static/js/0.fab4c4f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "321f3872ef21bc616db1",
+    "url": "./static/js/1.c1e1a3f1.chunk.js"
+  },
+  {
+    "revision": "d8d8e1a461bc247fbd37ad846147df80",
+    "url": "./static/js/1.c1e1a3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00d4968470c9eb22447f",
+    "url": "./static/js/10.e0b70bb1.chunk.js"
+  },
+  {
+    "revision": "11fd66ff51ed00dfb54c",
+    "url": "./static/js/4.59313737.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/4.59313737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74476acde065c131093b",
+    "url": "./static/js/5.68e2ff18.chunk.js"
+  },
+  {
+    "revision": "0d3ffcab9d88f730c9df",
+    "url": "./static/js/6.07f7d105.chunk.js"
+  },
+  {
+    "revision": "a5f482c82ba4404df23b",
+    "url": "./static/js/7.56e1c7a2.chunk.js"
+  },
+  {
+    "revision": "0a7502b4f538fdeed764",
+    "url": "./static/js/8.2083827b.chunk.js"
+  },
+  {
+    "revision": "5c643473bd56390ba0b4",
+    "url": "./static/js/9.cfcd3033.chunk.js"
+  },
+  {
+    "revision": "8d43696490ca4228dd4c",
+    "url": "./static/js/main.826fa027.chunk.js"
+  },
+  {
+    "revision": "09a62dddf512d5425bbb",
+    "url": "./static/js/runtime-main.4d4a298b.js"
+  },
+  {
+    "revision": "e4da2b738cbca0d69fc0301ee4d56a04",
+    "url": "./static/media/Untitled drawing.e4da2b73.jpg"
+  }
+]);
